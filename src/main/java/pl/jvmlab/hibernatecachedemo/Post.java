53e3113d174dd8f190f9f501f8c53a69f0e6fefe
grzeg <grzegorz.sowa314@gmail.com>
2021-04-28 23:32:46 +0200

    Post implements Persistable

--- src/main/java/pl/jvmlab/hibernatecachedemo/Post.java
+++ src/main/java/pl/jvmlab/hibernatecachedemo/Post.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.util.UUID;
 
 import static lombok.AccessLevel.PRIVATE;
@@ -17,16 +19,19 @@ import static lombok.AccessLevel.PRIVATE;
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Getter
 @NoArgsConstructor(access = PRIVATE)
-public class Post {
+public class Post implements Persistable<UUID> {
 
     @Id
     private UUID id;
+    @Transient
+    private boolean isNew;
     private UUID author;
     private String text;
 
     public static Post newInstance(UUID author, String text) {
         Post post = new Post();
         post.id = UUID.randomUUID();
+        post.isNew = true;
         post.author = author;
         post.text = text;
         return post;
