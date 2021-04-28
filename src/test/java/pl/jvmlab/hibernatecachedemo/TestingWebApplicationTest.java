356764a70f6345f7ad97066b7ccd413d1bdc04e5
grzeg <grzegorz.sowa314@gmail.com>
2021-04-28 23:33:11 +0200

    Added mvc test

---
+++ src/test/java/pl/jvmlab/hibernatecachedemo/TestingWebApplicationTest.java
@@ -0,0 +1,43 @@
+package pl.jvmlab.hibernatecachedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TestingWebApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String userId = UUID.randomUUID().toString();
+
+    @Test
+    public void shouldEditPost() throws Exception {
+
+        final String postId = this.mockMvc.perform(
+                post("/posts")
+                        .header("userId", userId)
+                .content("{\"text\": \"Sample post\"}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse().getContentAsString();
+
+        this.mockMvc.perform(
+                put("/posts/" + postId)
+                        .header("userId", userId)
+                        .content("{\"text\": \"Edited post\"}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+}
